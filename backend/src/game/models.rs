05081edba34a577f36fdfe89b3d46968125d1307
Henning Cullin <henningcullin@gmail.com>
2024-06-14 12:28:02 +0200

    feat: more models

--- backend/src/game/models.rs
+++ backend/src/game/models.rs
@@ -1,9 +1,14 @@
-pub struct Player {
-    pub color: Color,
-}
+use crate::auth::models::User;
 
 pub struct Piece {
     pub position: u32,
+}
+
+pub struct Pieces {
+    pub piece1: Piece,
+    pub piece2: Piece,
+    pub piece3: Piece,
+    pub piece4: Piece,
 }
 
 pub enum Color {
@@ -15,5 +20,19 @@ pub enum Color {
     Purple,
     Pink,
 }
+pub struct Player {
+    pub color: Color,
+    pub user: User,
+    pub pieces: Pieces,
+}
 
-pub struct Board {}
+pub struct Players {
+    pub player1: Player,
+    pub player2: Player,
+    pub player3: Player,
+    pub player4: Player,
+}
+
+pub struct Board {
+    pub players: Players,
+}
